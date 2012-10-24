bfbf4bd759c580fb5b38694d45717ef9e07af3e5
Uri London <u@london.org.il>
2012-10-24 23:44:06 +0200

    more fixr

--- fixr.py
+++ fixr.py
@@ -8,21 +8,49 @@ def run_cmd(cmd):
 def get_mac():
     lines = run_cmd( 'ipconfig /all' )
     state = 0
-    hmac = ''
+    mac = None
+    rx0 = re.compile('Connection-specific.*watchdox.com')
+    rx1 = re.compile('.*Physical Address.*: (.*)$')
     for ln in lines:
         if state == 0:
-            if re.search('Connection-specific.*watchdox.com',ln):
-                print ln,
+            if rx0.search(ln):
                 state = 1
         elif state == 1:
-            res = re.match('.*Physical Address.*: (.*)$',ln)
+            res = rx1.match(ln)
             if( res ):
-                print ln,
                 state = 2
-                hmac = res.groups()[0]
+                mac = res.groups()[0]
         elif state == 2:
-            break;
-    return hmac
+            break
+    return mac
 
 
-print get_mac()
+def get_intf(mac):
+    mac = ' '.join(mac.lower().split('-'))
+    rx0 = re.compile('Interface List')
+    rx1 = re.compile(' *(\d+)\.*' + mac + ' *\..*')
+    lines = run_cmd('route print')
+    state = 0
+    for ln in lines:
+        if state == 0:
+            if rx0.search(ln):
+                state = 1
+        elif state == 1:
+            res = rx1.match(ln)
+            if res:
+                state = 2
+                intf = res.groups()[0]
+        elif state == 2:
+            break
+    return intf
+
+
+
+if __name__ == '__main__':
+    mac = get_mac()
+    print mac
+    intf = get_intf(mac)
+    print intf
+
+
+
