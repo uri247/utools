6e1b634a363724873c1dc99238c0485300d5e971
unknown <uri@london.org.il>
2013-07-21 14:13:54 +0300

    skip java in ush.cmd

--- ush.cmd
+++ ush.cmd
@@ -19,11 +19,13 @@ rem
 
 
 :verify_java
-    if "%JAVA_HOME%"==""                       echo ERROR: JAVA_HOME must be defined && goto error
+    if "%JAVA_HOME%"==""                       echo WARNING: no JAVA settings && goto no_java
     if not exist "%JAVA_HOME%"                 echo ERROR: JAVA_HOME defined but %%^JAVA_HOME%% doesn't exist && goto error
     if not exist "%JAVA_HOME%\bin\javac.exe"   echo ERROR: JAVA_HOME doesn't point to a JDK distribution && goto error
     call javac -version >nul 2>&1
     if errorlevel 1                            echo ERROR: java compiler ^(javac.exe^) is not in path. && goto error
+
+:no_java
 
 
 :root
