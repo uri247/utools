050dbbfe528bbc82c056d5589141d8e5cd1b9929
Uri London <uri@london.org.il>
2014-09-23 00:54:44 -0700

    Check for dropbox

--- GetDboxFldr.py
+++ GetDboxFldr.py
@@ -1,11 +1,14 @@
-
 import json
 import os
 
 fn = r'%s\AppData\Roaming\Dropbox\info.json' % os.environ['UserProfile']
-with open(fn) as f:
-    d = json.load(f)
 
-dbox_fldr = d['personal']['path']
+if os.path.isfile(fn):
+    with open(fn) as f:
+        d = json.load(f)
+    dbox_fldr = d['personal']['path']
+
+else:
+    dbox_fldr = 'no dropbox'
 
 print dbox_fldr
