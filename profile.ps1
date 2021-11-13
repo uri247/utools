5637310892d69f9004696e1498c8f4ca6146ecc3
Uri London <Uri@Prisma.local>
2021-11-13 17:40:28 +0200

    The beginning of profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,23 @@
+# Uri's PowerShell profile
+#
+# Use this file by creating a symbolic link from where PowerShell is looking for it, to this file:
+#
+# New-Item -Type SymbolicLink -Path $PROFILE.CurrentUserAllHosts -Value C:\usr\utools\profile.ps1
+
+
+Set-Alias -Name tl -Value tasklist
+Set-Alias -Name tk -Value taskkill
+
+Set-Alias -Name chrm -Value "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"
+Set-Alias -Name uchrm -Value "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --user-data-dir=C:\Users\uri\uchrm
+
+Set-Alias -Name ww -Value "$env:ProgramFiles\Microsoft Office\root\Office16\WINWORD.EXE"
+Set-Alias -Name xl -Value "$env:ProgramFiles\Microsoft Office\root\Office16\EXCEL.EXE" $*
+Set-Alias -Name pp -Value "$env:ProgramFiles\Microsoft Office\root\Office16\POWERPNT.EXE" $*
+Set-Alias -Name ol -Value "$env:ProgramFiles\Microsoft Office\root\Office16\OUTLOOK.EXE" $*
+
+
+function make-link ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
